26518cf05384e77c61d7e96d3ed3b8ec93caef81
lingfliu <lingfeng.liu@163.com>
2023-03-03 21:53:49 +0800

    added

--- app/src/main/java/io/issc/android_dev_tutorial_kt/MainActivity.kt
+++ app/src/main/java/io/issc/android_dev_tutorial_kt/MainActivity.kt
@@ -3,6 +3,7 @@ package io.issc.android_dev_tutorial_kt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import io.issc.android_dev_tutorial_kt.databinding.ActivityMainBinding
