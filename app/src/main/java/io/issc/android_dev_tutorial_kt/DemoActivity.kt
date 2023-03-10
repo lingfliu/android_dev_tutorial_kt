453afa1186d59c73f582b1843a58ce7f05da649c
lingfliu <lingfeng.liu@163.com>
2023-03-10 14:00:44 +0800

    added demo activity

---
+++ app/src/main/java/io/issc/android_dev_tutorial_kt/DemoActivity.kt
@@ -0,0 +1,18 @@
+package io.issc.android_dev_tutorial_kt
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DemoActivity: AppCompatActivity() {
+    lateinit var
+    lateinit var btn:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        btn
+    }
+
+    override fun
+}
