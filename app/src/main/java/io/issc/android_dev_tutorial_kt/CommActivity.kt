5b57a1ba9a038bed7b817691076560e6f44cd075
lingfliu <lingfeng.liu@163.com>
2023-03-31 16:49:18 +0800

    updated

--- app/src/main/java/io/issc/android_dev_tutorial_kt/CommActivity.kt
+++ app/src/main/java/io/issc/android_dev_tutorial_kt/CommActivity.kt
@@ -8,10 +8,12 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import io.issc.android_dev_tutorial_kt.databinding.ActivityIoBinding
 import io.issc.android_dev_tutorial_kt.model.MessageBundle
+import io.issc.android_dev_tutorial_kt.model.ProjectInfo
 import io.issc.android_dev_tutorial_kt.model.ProjectInfoExt
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
 import io.ktor.client.features.*
+import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.*
 import java.net.Socket
@@ -31,13 +33,44 @@ class CommActivity : AppCompatActivity() {
     var bSocket:BluetoothSocket? = null
     lateinit var bAdapter:BluetoothAdapter
 
+    suspend fun testFun() {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIoBinding.inflate(layoutInflater)
         setContentView(binding.root)
         btn = binding.btn
 
+//        Thread{
+//            //TODO
+//            val a = 0
+//        }.start()
+
+//        //runblocking 协程启动
+//        runBlocking {
+//            withContext(Dispatchers.IO) {
+//                val a = 0
+//            }
+//        }
+
+//        // 启动一个协程
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val a = 0
+//        }
+
+
+
         btn.setOnClickListener{
+            // get demo without use keyword
+//            runBlocking {
+//                val httpClient = HttpClient(Android)
+//                val paramName = "tn"
+//                val paramValue = "78040160_1_dg"
+//                val response = httpClient.get<String>("https://www.baidu.com/?$paramName=$paramValue")
+//                Log.d("ioacti", response)
+//            }
 
             //get请求demo
 //            runBlocking {
@@ -48,30 +81,33 @@ class CommActivity : AppCompatActivity() {
 //            }
 
             //restful get 请求demo
-//            runBlocking {
-//                HttpClient(Android){
-//                    install(io.ktor.client.features.json.JsonFeature){
-//                        serializer = io.ktor.client.features.json.GsonSerializer{
-//                            serializeNulls()
-//                            disableHtmlEscaping()
+//            coroutineTester.submit(object: CoroutineTester.Task {
+//                override suspend fun run() {
+//                    HttpClient(Android) {
+//                        install(JsonFeature) {
+//                            serializer = GsonSerializer {
+//                                serializeNulls()
+//                                disableHtmlEscaping()
+//                            }
 //                        }
 //                    }
+//                        .use {
+//                            val response =
+//                                it.get<MessageBundle<List<ProjectInfo>>>("http://plaf.kingroad.com:8059/api/pdb/project/info/all")
+//                            Log.d("ioacti", response.message.toString())
+//                        }
 //                }
-//                    .use {
-//                        val response = it.get<MessageBundle<List<ProjectInfo>>>("http://plaf.kingroad.com:8059/api/pdb/project/info/all")
-//                        Log.d("ioacti", response.message.toString())
-//                    }
-//            }
+//            })
 
-            coroutineTester.submit(object: CoroutineTester.Task {
-                override suspend fun run() {
-                    HttpClient(Android).use {
-                        val response = it.get<String>("https://www.baidu.com")
-                        Log.d("ioacti", response)
-                    }
-                }
-            })
-//
+//            coroutineTester.submit(object: CoroutineTester.Task {
+//                override suspend fun run() {
+//                    HttpClient(Android).use {
+//                        val response = it.get<String>("https://www.baidu.com")
+//                        Log.d("ioacti", response)
+//                    }
+//                }
+//            })
+
 //            coroutineTester.submit(object: CoroutineTester.Task {
 //
 //                override suspend fun run() {
@@ -86,27 +122,29 @@ class CommActivity : AppCompatActivity() {
 //            }
 
 
-            //restful post 请求demo
-            runBlocking {
-                HttpClient(Android){
-                    defaultRequest {
-                        header("Content-Type", "application/json")
-                    }
+//            //restful post 请求demo
+//            runBlocking {
+//                HttpClient(Android){
+//                    defaultRequest {
+//                        header("Content-Type", "application/json")
+//                        header("Authorization","auth123")
+//                    }
+//
+//                    install(JsonFeature){
+//                        serializer = GsonSerializer{
+//                            serializeNulls()
+//                            disableHtmlEscaping()
+//                        }
+//                    }
+//                }
+//                    .use {
+//                        val response = it.post<MessageBundle<ProjectInfoExt>>("http://plaf.kingroad.com:8059/api/pdb/project/ext/info/update"){
+//                            body = ProjectInfoExt("1480853307463585800", 12.00000, 12.0000 )
+//                        }
+//                        Log.d("ioacti", response.toString())
+//                    }
+//            }
 
-                    install(io.ktor.client.features.json.JsonFeature){
-                        serializer = io.ktor.client.features.json.GsonSerializer{
-                            serializeNulls()
-                            disableHtmlEscaping()
-                        }
-                    }
-                }
-                    .use {
-                        val response = it.post<MessageBundle<ProjectInfoExt>>("http://plaf.kingroad.com:8059/api/pdb/project/ext/info/update"){
-                            body = ProjectInfoExt("1480853307463585800", 12.00000, 12.0000 )
-                        }
-                        Log.d("ioacti", response.message.toString())
-                    }
-            }
         }
 
 //        //蓝牙demo
@@ -158,13 +196,11 @@ class CommActivity : AppCompatActivity() {
             //socket client demo
             runBlocking {
                 withContext(Dispatchers.IO) {
-                    val socket = Socket("10.0.2.2", 9009)
+                    val socket = Socket("10.0.2.2", 9009) //
                     socket.soTimeout = 10000
 //                    socket.connect(socket.remoteSocketAddress, 10000)
-                    socket.let {
-                        it.getOutputStream().write("hello from android".toByteArray())
-//                        it.getInputStream().read()
-                    }
+                    val bytes = socket.getInputStream().readBytes()
+                    socket.getOutputStream().write("hello from android".toByteArray())
                     socket.close()
                 }
             }
